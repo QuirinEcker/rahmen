9423eae55827753df9b3e05229711f6a1fac0b19
QuirinEcker <quirin.ecker@bajupa.com>
2017-12-17 14:06:41 +0100

    finish code

--- src/main/java/Ecker_Rahmen.java
+++ src/main/java/Ecker_Rahmen.java
@@ -1,6 +1,67 @@
+/*----------------------------------------------------------
+ *                 HTL-Leonding / Klasse: 1xHITM
+ * ----------------------------------------------------------
+ * Übungsnr.:   --
+ * Übungstitel: Rahmen
+ * Autoren:     Ecker Quirin
+ * Version:     1.0
+ * Datum:       17.12.2017
+ * ----------------------------------------------------------
+ * Kurzbeschreibung:
+ * Rahmen um eine Zahl.
+ *
+ * ----------------------------------------------------------
+ * Beschreibung der grundlegenden Lösungsidee:
+ * Anzahl der Ziffern ausrechnen und Sterne ausgeben.
+ *
+ * ----------------------------------------------------------
+*/
+
+import java.util.Scanner;
+
 public class Ecker_Rahmen {
 
     public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
 
+        int number;
+        int digitCount = 0;
+        int numberToDivide;
+
+        System.out.print("Zahl: ");
+        number = scanner.nextInt();
+
+        numberToDivide = number;
+
+        //DigitCount Calculate
+        for (digitCount = 0; numberToDivide > 0; digitCount++){
+            numberToDivide /= 10;
+        }
+
+        //Border-Top
+        for (int i = digitCount + 4;i != 0;i--){
+            System.out.print("*");
+        }
+        System.out.println("");
+        System.out.print("**");
+        for (int i = digitCount; i > 0; i--){
+            System.out.print(" ");
+        }
+        System.out.print("**");
+        System.out.println();
+
+
+        //Border Mid
+        System.out.println("* " + number + " *");
+
+        //Border Buttom
+        System.out.print("**");
+        for (int i = digitCount; i > 0; i--){
+            System.out.print(" ");
+        }
+        System.out.println("**");
+        for (int i = digitCount + 4;i != 0;i--){
+        System.out.print("*");
+        }
     }
 }
